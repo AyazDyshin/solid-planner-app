59248e4b400ad7de14fb6f37cb159961841a13fc
Shad <ayazdyshin@gmail.com>
2022-04-20 22:02:49 +0200

    add styling to log in page

--- src/components/LogInPage.tsx
+++ src/components/LogInPage.tsx
@@ -6,7 +6,7 @@ import {
   logout,
   getDefaultSession,
 } from "@inrupt/solid-client-authn-browser";
-
+import { Button, Card, InputGroup, Dropdown, DropdownButton, FormControl } from 'react-bootstrap';
 interface Props {
   logInStatus: boolean;
   setLogInStatus: React.Dispatch<React.SetStateAction<boolean>>;
@@ -14,7 +14,7 @@ interface Props {
 const LogInPage = ({ logInStatus, setLogInStatus }: Props) => {
 
   const [webId, setWebId] = useState<string | undefined>(getDefaultSession().info.webId);
-  const [issuer, setIssuer] = useState<string>("https://inrupt.net");
+  const [issuer, setIssuer] = useState<string>("");
   const [resource, setResource] = useState<string | undefined>(webId);
 
   function handleLogin(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
@@ -30,24 +30,40 @@ const LogInPage = ({ logInStatus, setLogInStatus }: Props) => {
   };
 
   return (
-    <div className="d-flex align-items-center justify-content-center min-vh-100">
-      <main>
-        <h1>Solid Planner App</h1>
-        <div>
-          <form>
-            <input
-              type="text"
-              value={issuer}
-              onChange={(e) => {
-                setIssuer(e.target.value);
-              }}
-            />
-            <button onClick={(e) => handleLogin(e)}>Log In</button>
-          </form>
-        </div>
-      </main>
-    </div>
-  );
+      <div>
+        <h3 className="display-3 text-center mt-4">Solid Planner App</h3>
+      <div className="position-absolute top-50 start-50 translate-middle ">
+
+        <InputGroup>
+          <div className="row">
+          <DropdownButton
+            variant="outline-secondary"
+            title="Choose a provider"
+            id="input-group-dropdown-1"
+          >
+            <Dropdown.Item href="#" onClick={() => {
+              setIssuer("https://inrupt.net");
+            }}>inrupt.net</Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item href="#">Something else here</Dropdown.Item>
+            <Dropdown.Item href="#">Separated link</Dropdown.Item>
+          </DropdownButton>
+          <FormControl aria-label="Text input with dropdown button" value={issuer}
+            onChange={(e) => {
+              setIssuer(e.target.value);
+            }} />
+            </div>
+        </InputGroup>
+
+        <div className="row">
+          <Button onClick={(e) => handleLogin(e)}>Log In</Button>
+          </div>
+      </div>
+      </div>
+    
+  )
 }
 
+
 export default LogInPage;
+

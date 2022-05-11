6660c16524544f2c9463d8bf88dfccb710326c60
Shad <ayazdyshin@gmail.com>
2022-05-11 17:28:45 +0200

    NavBar.tsx cleanup, add comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
 import { LogoutButton, useSession } from "@inrupt/solid-ui-react";
 import { Button } from "react-bootstrap";
-interface Props{
-    links : string[];
+
+interface Props {
+    links: string[];
     active: string;
     setActive: React.Dispatch<React.SetStateAction<string>>;
 }
+// this component renders NavBar, it iterates over "links" array and creates corresponding links
+// clicking on the links sets "active" to the links value
+// logout functionality is implemented using "LogoutButton" component from @inrupt/solid-ui-react
+const Navbar = ({ links, active, setActive }: Props) => {
 
-const Navbar = ({links, active, setActive}: Props) => {
-   
     const onError = (error: Error) => {
         console.log(error);
-      }
-    return (
+    }
 
+    return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-
                 <div className="navbar-nav d-flex justify-content-around w-100">
-
-
                     {links.map((link) => (
                         <a
                             key={`${link}${Date.now()}`}
@@ -28,17 +27,15 @@ const Navbar = ({links, active, setActive}: Props) => {
                                 e.preventDefault();
                                 setActive(link)
                             }}
-                
+
                         >{link}</a>
                     ))}
-
                     <LogoutButton onError={onError} >
                         <Button variant="secondary">Log Out</Button>
                     </LogoutButton>
                 </div>
             </div>
         </nav>
-
     )
 }
 

ed2d670d04d4dd5b7ca1001b82e890dad6cd34f0
Shad <ayazdyshin@gmail.com>
2022-05-11 17:24:13 +0200

    MainContent.tsx cleanup, add comments

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,40 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { notEqual } from "assert";
-import { Note } from './types';
-import { LogoutButton, useSession } from "@inrupt/solid-ui-react";
+import { useState } from "react";
 import Navbar from "./Navbar";
 import ContentToRender from "./ContentToRender";
 import Test from "./Test";
 
-interface Props {
-  
-}
-
+// This is the root component that first renders NavBar and then other content
+// Passes active and setActive hooks, which represent the currently clicked tab
 const MainContent = () => {
-  // is there a better practice to handle this passing of an empty note?
-  const [note, setNote] = useState<Note>({id:0, title:"", content:""});
-  const [notes, setNotes] = useState<Note[]>([]);
+
   const links = ['notes', 'categories', 'habits'];
   const [active, setActive] = useState("notes");
-  const { session, fetch} = useSession();
-  const { webId } = session.info;
-  
-  const handleAdd = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    
-    // if(note){ 
-    //   setNotes([...notes, {id:  Date.now(), title: note.title, content: note.content}]);
-    //   setNote({id:0, title:"", content:""});
-    // }
-  };
 
   return (
     <div>
       <Test />
-      <Navbar links={links} active={active} setActive={setActive} /> 
-      <ContentToRender active={active}/>   
+      <Navbar links={links} active={active} setActive={setActive} />
+      <ContentToRender active={active} />
     </div>
-
   );
 };
 

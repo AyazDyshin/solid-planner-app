4a013e40bcbeebfd62024f6da03ea2da7e6166fd
Shad <ayazdyshin@gmail.com>
2022-05-12 23:33:39 +0200

    add and implement note edit

--- src/components/NoteCreator.tsx
+++ src/components/NoteCreator.tsx
@@ -1,6 +1,5 @@
 import { Thing } from "@inrupt/solid-client";
 import { useSession } from "@inrupt/solid-ui-react";
-import { view } from "rdf-namespaces/dist/hydra";
 import { useEffect, useState } from "react";
 import { InputGroup, FormControl, Button } from "react-bootstrap";
 import { saveNote, thingToNote } from "../services/SolidPod";
@@ -16,17 +15,18 @@ interface Props {
 }
 //component of creation and saving a note the user's pod
 const NoteCreator = ({ newEntryCr, setNewEntryCr, thingToView, setThingToView, viewerStatus, setViewerStatus }: Props) => {
-    
+
     useEffect(() => {
         if (viewerStatus) {
             // handle 
             setNoteInp(thingToNote(thingToView!));
         }
-    }, [viewerStatus,thingToView]);
+    }, [viewerStatus, thingToView]);
 
     const { session, fetch } = useSession();
     const { webId } = session.info;
     const [NoteInp, setNoteInp] = useState<Note>({ id: null, title: "", content: "" });
+    const [isEdit, setIsEdit] = useState(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNoteInp(prevState => ({ ...prevState, [e.target.name]: e.target.value }))
@@ -40,26 +40,32 @@ const NoteCreator = ({ newEntryCr, setNewEntryCr, thingToView, setThingToView, v
         setViewerStatus(false);
     };
 
+    const handleEdit = () => {
+        isEdit ? setIsEdit(false) : setIsEdit(true);
+    }
     return (
         <div>
             <div className="h-100">
                 <InputGroup className="mb-2 mt-2">
-                    <InputGroup.Text id="basic-addon1">Title:</InputGroup.Text>
+                    <InputGroup.Text {...(!isEdit && { disabled: true })} id="basic-addon1">Title:</InputGroup.Text>
                     <FormControl
                         name="title"
                         aria-label="title"
                         value={NoteInp.title}
                         onChange={handleChange} />
                 </InputGroup>
-                <FormControl as="textarea" aria-label="textarea" style={{ 'resize': 'none', 'height': '91%' }}
+                <FormControl {...(!isEdit && { disabled: true })} as="textarea" aria-label="textarea" style={{ 'resize': 'none', 'height': '91%' }}
                     name="content"
                     value={NoteInp.content}
                     onChange={handleChange}
                 />
             </div>
             <Button onClick={handleSave}>save</Button>
+            {viewerStatus && <Button onClick={handleEdit}>edit</Button>}
+            <Button>delete</Button>
         </div>
     )
 }
 
 export default NoteCreator;
+
